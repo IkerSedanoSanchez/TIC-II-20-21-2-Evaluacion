4737b5b017b0e89f932b6db1a885ad8693ae3050
IkerSedanoSanchez <71393387+IkerSedanoSanchez@users.noreply.github.com>
2021-01-27 09:44:48 +0100

    Add files via upload

---
+++ Puntero 2.cpp
@@ -0,0 +1,15 @@
+#include<stdio.h>
+//¿Y los arrays?
+int main(){
+	char letras[10];
+	int cont;
+	letras[0]='H';
+	letras[1]='O';
+	letras[2]='L';
+	letras[3]='A';
+	for(cont=0;cont<10;cont++){
+		printf("%C",*(letras+cont));
+		//printf("%c,letras[cont]);
+	}
+	return 0;
+}
